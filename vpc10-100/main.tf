40e5b0051bd584d114a821d543bff574f0e996a8
yungpc <teddy.yung@gmail.com>
2022-01-23 09:56:03 -0500

    update module source to github repo

--- vpc10-100/main.tf
+++ vpc10-100/main.tf
@@ -1,7 +1,7 @@
 
 #=========================VPC10-100 Calling Module==========================
 module "vpc10-100" {
-   source = "../../../modules/vpc"
+   source = "git::https://github.com/yungpc/terraform-aws-module/tree/main/vpc"
    vpc_cidr = "10.100.0.0/16"
    vpc_name = "vpc-10-100"
    subnet_private_a = "10.100.1.0/24"
